d701a9e2c6de20e89042130d82b9b25de9958686
Hayden <hayden.barnes@suse.com>
2021-11-11 12:37:06 -0500

    Add GRUB labels

--- crj.sh
+++ crj.sh
@@ -2,3 +2,5 @@
 source crj.config
 cp scripts/default_config.sh scripts/config.sh
 sed -i -E "s/ubuntu-from-scratch/$ISOFILENAME/g" scripts/config.sh
+sed -i -E "s/Try Ubuntu FS without installing/$GRUBTRYLABEL/g" scripts/config.sh
+sed -i -E "s/Install Ubuntu FS/$GRUBINSTALLLABEL/g" scripts/config.sh
